c598cbc7859fd76bea51dddf8cca74f4ae5a20d2
Rajat <rajat.kumar@medleymed.com>
2021-10-14 19:24:04 +0530

    changed function based view to class based view

--- movies/urls.py
+++ movies/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from movies import views
 
 urlpatterns = [
-	path('view_movies/<str:city>/',views.view_movies),
-	path('view_cinemas/<int:movie_id>/',views.view_cinemas),
-	path('view_seats/<int:showtime_id>/',views.view_seats),
-	path('book_ticket/',views.book_ticket)
+	path('view_movies/<str:city>/',views.MovieView.as_view()),
+	path('view_cinemas/<int:movie_id>/',views.CinemaView.as_view()),
+	path('view_seats/<int:showtime_id>/',views.SeatView.as_view()),
+	path('book_ticket/',views.TicketView.as_view())
 ]
--- movies/views.py
+++ movies/views.py
@@ -4,52 +4,54 @@ from movies.serializers import (MovieSerializer, CinemaSerializer, ShowTimeSeria
 		TicketSerializer)
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 
-@api_view(('GET',))
-def view_movies(request, city):
-	try:
-		movies = Movie.objects.prefetch_related("cinema").filter(
-			cinema__city__name=city).distinct()
-		serializer = MovieSerializer(movies, many=True)
-		return Response(serializer.data)
-	except Exception as e:
-		print (e)
-		return Response({"message":"Something went wrong!!"})
 
-@api_view(('GET',))
-def view_cinemas(request, movie_id):
-	try:
-		cinema = Cinema.objects.filter(movies=movie_id)
-		serializer = CinemaSerializer(cinema, many=True)
-		return Response(serializer.data)
-	except Exception as e:
-		print (e)
-		return Response({"message":"Something went wrong!!"})
-
-@api_view(('GET',))
-def view_seats(request, showtime_id):
-	try:
-		show = ShowTime.objects.get(id=showtime_id)
-		serializer = ShowTimeSerializer(show)
-		return Response(serializer.data)
-	except Exception as e:
-		print (e)
-		return Response({"message":"Something went wrong!!"})
-
-@api_view(('POST',))
-def book_ticket(request):
-	try:
-		serializer = TicketSerializer(data=request.data)
-		if serializer.is_valid():
-			validated_data  =serializer.validated_data
-			if not Ticket.objects.filter(showtime=validated_data["showtime"],
-				seat_number=validated_data["seat_number"]):
-				serializer.save()
-			else:
-				return Response({"message":"This seat is already booked"})
+class MovieView(APIView):
+	def get(self, request, city):
+		try:
+			movies = Movie.objects.prefetch_related("cinema").filter(
+				cinema__city__name=city).distinct()
+			serializer = MovieSerializer(movies, many=True)
 			return Response(serializer.data)
-		else:
-			return Response(serializer.errors)
-	except Exception as e:
-		print (e)
-		return Response({"message":"Something went wrong!!"})
+		except Exception as e:
+			print (e)
+			return Response({"message":"Something went wrong!!"})
+
+class CinemaView(APIView):
+	def get(self, request, movie_id):
+		try:
+			cinema = Cinema.objects.filter(movies=movie_id)
+			serializer = CinemaSerializer(cinema, many=True)
+			return Response(serializer.data)
+		except Exception as e:
+			print (e)
+			return Response({"message":"Something went wrong!!"})
+
+class SeatView(APIView):
+	def get(self, request, showtime_id):
+		try:
+			show = ShowTime.objects.get(id=showtime_id)
+			serializer = ShowTimeSerializer(show)
+			return Response(serializer.data)
+		except Exception as e:
+			print (e)
+			return Response({"message":"Something went wrong!!"})
+
+class TicketView(APIView):
+	def post(self,request):
+		try:
+			serializer = TicketSerializer(data=request.data)
+			if serializer.is_valid():
+				validated_data  =serializer.validated_data
+				if not Ticket.objects.filter(showtime=validated_data["showtime"],
+					seat_number=validated_data["seat_number"]):
+					serializer.save()
+				else:
+					return Response({"message":"This seat is already booked"})
+				return Response(serializer.data)
+			else:
+				return Response(serializer.errors)
+		except Exception as e:
+			print (e)
+			return Response({"message":"Something went wrong!!"})
